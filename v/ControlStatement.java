81dca090afe502563aa9428ab1af6232f6bad020
Kushalcodehub <kushalverma5999@gmail.com>
2022-03-23 10:58:47 +0530

    Hello buddy

---
+++ v/ControlStatement.java
@@ -0,0 +1,12 @@
+package k.v;
+
+public class ControlStatement
+{
+  public static void main(String args[])
+  {int a=40;
+  if(a>33)
+  {
+	System.out.println("Passed"); 
+  }
+  }
+}
---
+++ v/Hello.java
@@ -0,0 +1,10 @@
+package k.v;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	   System.out.println("Kushal");
+
+	}
+
+}
